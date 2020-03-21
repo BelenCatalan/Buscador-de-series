822b65b85c0a40dfdcca709b168402888143d134
BelenCatalan <labdelante@gmail.com>
2020-03-21 09:38:35 +0100

    changes index and css

--- js/main.js
+++ js/main.js
@@ -23,27 +23,26 @@ function getApiSeries(ev) {
         // console.log(search.show.name);
         // console.log(search.show.image.medium);
         let seriesSearch = { id: search.show.id, name: search.show.name, image: search.show.image };
-        console.log(seriesSearch);
+        // console.log(seriesSearch);
         series.push(seriesSearch);
         paintSeries();
       }
-      console.log(series);
+      // console.log(series);
     });
 }
 
 btn.addEventListener('click', getApiSeries);
-console.log(series);
+// console.log(series);
 
 function getSeriesHtmlCode(serie) {
   let htmlCode = '';
-  htmlCode += `<li class="list__style">`;
+  htmlCode += `<li class="list__style" id="${serie.id}">`;
   if (serie.image === null) {
-    htmlCode += `  <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV.'" class="" alt="Serie: ${serie.name}">`;
+    htmlCode += `  <img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV.'" class="js-add-series" alt="Serie: ${serie.name}" id="${serie.id}">`;
   } else {
-    htmlCode += `  <img src="${serie.image.medium}" class="" alt="Serie: ${serie.name}">`;
+    htmlCode += `  <img src="${serie.image.medium}" class="js-add-series" alt="Serie: ${serie.name}" id="${serie.id}">`;
   }
-
-  htmlCode += `  <h3>${serie.name}</h3>`;
+  htmlCode += `  <p>${serie.name}</p>`;
   htmlCode += `  </li>`;
   return htmlCode;
 }
@@ -54,13 +53,40 @@ function paintSeries() {
   }
   const seriesElements = document.querySelector('.js-series-elements');
   seriesElements.innerHTML = seriesCode;
+  listenAddSeries();
 }
-
-// function listenAddSeries() {}
+// listenAddSeries();
+function listenAddSeries() {
+  const seriesAddBtns = document.querySelectorAll('.js-add-series');
+  console.log(seriesAddBtns);
+  for (const seriesaddBtn of seriesAddBtns) {
+    seriesaddBtn.addEventListener('click', addFavoriteArray);
+  }
+}
 
 // function addFavoriteStyle() {}
 
-// function addFavoriteArray() {}
+function addFavoriteArray(ev) {
+  debugger;
+  const clickedId = ev.target.id;
+  console.log(clickedId);
+  let foundSerie;
+  for (const favoriteSerie of favorites) {
+  }
+  if (foundSerie === undefined) {
+    // si no está en favorites
+    // busco el el array series el id
+    let foundSeries = findInArray(clickedId, series);
+    // añado todo el objeto al array favorites
+    favorites.push({
+      id: foundSeries.id,
+      name: foundSeries.name,
+      image: foundSeries.image
+    });
+  } else {
+  }
+  console.log(favorites);
+}
 
 // function deleteFavoriteArray() {}
 
